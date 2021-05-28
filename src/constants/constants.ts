43344a0260721e49609b55d3a16b5ca89cf02b42
Andrey Lazarev <andrey.lazarev34@gmail.com>
2021-05-28 13:43:45 +0400

    add constants/

---
+++ src/constants/constants.ts
@@ -0,0 +1,15 @@
+import { TVector } from '../types/types';
+
+export const VECTOR_MOCKUP: TVector = [
+	{ x: 100, y: 200 },
+	{ x: 120, y: 100 },
+	{ x: 170, y: 300 },
+	{ x: 140, y: 250 },
+	{ x: 150, y: 400 },
+	{ x: 150, y: 400 },
+	{ x: 400, y: 280 }
+];
+
+export const SCATTER_FILL_COLOR = '#8884d8';
+export const SCATTER_SIDE_LENGTH = 400;
+export const SCATTER_MARGIN = 20;
