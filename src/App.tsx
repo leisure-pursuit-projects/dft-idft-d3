d5902bd7d57ae06ec35c84f4a53a141da606a248
Andrey Lazarev <andrey.lazarev34@gmail.com>
2021-06-03 21:34:41 +0400

    add button, minor slider updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CartesianGrid, Scatter, ScatterChart, XAxis, YAxis, } from 'recharts';
 import {
 	SCATTER_FILL_COLOR,
@@ -9,41 +9,20 @@ import {
 import { EScatterName } from './types/types';
 import { Slider, SliderFilledTrack, SliderThumb, SliderTrack } from '@chakra-ui/slider';
 import { Box } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/react';
 import './App.css';
 
 export function App () {
-	return (
-		<div className={ 'app' }>
-			{
-				Object.values(EScatterName).map((name) => (
-					<ScatterChart
-						key={ name }
-						width={ SCATTER_SIDE_LENGTH }
-						height={ SCATTER_SIDE_LENGTH }
-						margin={ {
-							top: SCATTER_MARGIN,
-							right: SCATTER_MARGIN,
-							bottom: SCATTER_MARGIN,
-							left: SCATTER_MARGIN,
-						} }
-					>
-						<CartesianGrid/>
-						<XAxis type="number" dataKey="x"/>
-						<YAxis type="number" dataKey="y"/>
-						<Scatter
-							name={ name }
-							data={ VECTOR_MOCKUP }
-							fill={ SCATTER_FILL_COLOR }
-						/>
-					</ScatterChart>
-				))
-			}
+	const [lossPercent, setLossPercent] = useState(0);
+
+	const SLIDER = (
+		<>
 			<Slider
-				defaultValue={ 0 }
+				defaultValue={ lossPercent }
 				min={ 0 }
 				max={ 100 }
 				step={ 1 }
-				onChangeEnd={ console.error }
+				onChangeEnd={ setLossPercent }
 			>
 				<SliderTrack bg={ 'lightgrey' }>
 					<Box
@@ -54,6 +33,50 @@ export function App () {
 				</SliderTrack>
 				<SliderThumb boxSize={ 4 }/>
 			</Slider>
+			<p>{ `Loss value is set to ${ lossPercent }%` }</p>
+		</>
+	)
+
+	const BUTTON = (
+		<Button onClick={ console.error }>
+			Generate new vector
+		</Button>
+	)
+
+	return (
+		<div className={ 'app' }>
+			{
+				Object.values(EScatterName).map((name) => (
+					<div
+						key={ name }
+						className={ 'scatter-chart-container' }
+					>
+						<ScatterChart
+							width={ SCATTER_SIDE_LENGTH }
+							height={ SCATTER_SIDE_LENGTH }
+							margin={ {
+								top: SCATTER_MARGIN,
+								right: SCATTER_MARGIN,
+								bottom: SCATTER_MARGIN,
+								left: SCATTER_MARGIN,
+							} }
+						>
+							<CartesianGrid/>
+							<XAxis type="number" dataKey="x"/>
+							<YAxis type="number" dataKey="y"/>
+							<Scatter
+								name={ name }
+								data={ VECTOR_MOCKUP }
+								fill={ SCATTER_FILL_COLOR }
+							/>
+						</ScatterChart>
+						<p>
+							{ name }
+						</p>
+						{ name === 'DFT' ? BUTTON : SLIDER }
+					</div>
+				))
+			}
 		</div>
 	)
 }
