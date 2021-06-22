82393280cb89fe133e25ccb7e465125f35e2174d
Andrey Lazarev <andrey.lazarev34@gmail.com>
2021-06-22 05:11:59 +0400

    update constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,13 +1,13 @@
 import { ComplexVector } from '../types/types';
 
 export const VECTOR_MOCKUP: ComplexVector = [
-	{ re: 100, im: 200 },
-	{ re: 120, im: 100 },
-	{ re: 170, im: 300 },
-	{ re: 140, im: 250 },
-	{ re: 150, im: 400 },
-	{ re: 150, im: 400 },
-	{ re: 400, im: 280 }
+	{ x: 100, y: 200 },
+	// { re: 120, im: 100 },
+	// { re: 170, im: 300 },
+	// { re: 140, im: 250 },
+	// { re: 150, im: 400 },
+	// { re: 150, im: 400 },
+	// { re: 400, im: 280 }
 ];
 
 export const SCATTER_FILL_COLOR = '#8884d8';
