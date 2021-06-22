4bead9878908d0cbc551c2e999d9c065e996b107
Andrey Lazarev <andrey.lazarev34@gmail.com>
2021-06-22 05:12:54 +0400

    (WIP) testing fft

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,105 @@
-import React, {useEffect, useState} from 'react';
-import {CartesianGrid, Scatter, ScatterChart, XAxis, YAxis,} from 'recharts';
-import {SCATTER_FILL_COLOR, SCATTER_MARGIN, SCATTER_SIDE_LENGTH, VECTOR_MOCKUP} from './constants/constants';
-import {FastFourierTransforms} from './types/types';
+import React, { useState } from 'react';
+import { CartesianGrid, Scatter, ScatterChart, XAxis, YAxis } from 'recharts';
+import { SCATTER_FILL_COLOR, SCATTER_MARGIN, SCATTER_SIDE_LENGTH, VECTOR_MOCKUP } from './constants/constants';
+import { FastFourierTransforms } from './types/types';
 import './App.css';
-import {getGlobalWasmState, getWasmLibIsLoaded} from './wasm/wasm-loader';
-import {Slider, SliderFilledTrack, SliderThumb, SliderTrack} from '@chakra-ui/slider';
-import {Button} from '@chakra-ui/button';
-import {Box} from '@chakra-ui/layout';
+import { Slider, SliderFilledTrack, SliderThumb, SliderTrack } from '@chakra-ui/slider';
+import { Button } from '@chakra-ui/button';
+import { Box } from '@chakra-ui/layout';
+import { FFT } from './fft/fft';
 
 export function App() {
 	const [lossPercent, setLossPercent] = useState(0);
-	const [wasmGreeting, setWasmGreeting] = useState('');
-	const [wasmLoadStatus, setWasmLoadStatus] = useState<boolean | 'failed'>(false);
+	// const [wasmGreeting, setWasmGreeting] = useState('');
+	// const [wasmLoadStatus, setWasmLoadStatus] = useState<boolean | 'failed'>(false);
+	//
+	// const handleClick = () => {
+	// 	const wasm = getWasmLibIsLoaded();
+	// 	if (wasm) {
+	// 		setWasmGreeting(wasm.get_greeting());
+	// 	}
+	// };
+	//
+	// useEffect(() => {
+	// 	const waitForWasm = async () => {
+	// 		const {promise} = getGlobalWasmState();
+	// 		await promise;
+	// 		const {failedToLoad} = getGlobalWasmState();
+	// 		setWasmLoadStatus(failedToLoad ? 'failed' : true);
+	// 	};
+	// 	waitForWasm().catch(console.error);
+	// }, []);
 
 	const handleClick = () => {
-		const wasm = getWasmLibIsLoaded();
-		if (wasm) {
-			setWasmGreeting(wasm.get_greeting());
-		}
+		console.error(FFT([0, 1, 2, 1 ,1, 2, 1, 1]));
 	};
-
-	useEffect(() => {
-		const waitForWasm = async () => {
-			const {promise} = getGlobalWasmState();
-			await promise;
-			const {failedToLoad} = getGlobalWasmState();
-			setWasmLoadStatus(failedToLoad ? 'failed' : true);
-		};
-		waitForWasm().catch(console.error);
-	}, []);
 
 	const SLIDER = (
 		<>
 			<Slider
-				defaultValue={lossPercent}
-				min={0}
-				max={100}
-				step={1}
-				onChangeEnd={setLossPercent}
+				defaultValue={ lossPercent }
+				min={ 0 }
+				max={ 100 }
+				step={ 1 }
+				onChangeEnd={ setLossPercent }
 			>
-				<SliderTrack bg={'lightgrey'}>
+				<SliderTrack bg={ 'lightgrey' }>
 					<Box
-						position={'relative'}
-						right={10}
+						position={ 'relative' }
+						right={ 10 }
 					/>
 					<SliderFilledTrack bg="grey"/>
 				</SliderTrack>
-				<SliderThumb boxSize={4}/>
+				<SliderThumb boxSize={ 4 }/>
 			</Slider>
-			<p>{ `Loss value is set to ${lossPercent}%` }</p>
+			<p>{ `Loss value is set to ${ lossPercent }%` }</p>
 		</>
 	)
 
 	const BUTTON = (
 		<Button
-			disabled={wasmLoadStatus === false || wasmLoadStatus === 'failed'}
-			onClick={handleClick}
+			// disabled={ wasmLoadStatus === false || wasmLoadStatus === 'failed' }
+			onClick={ handleClick }
 		>
-			Generate new vector {wasmGreeting}
+			Generate new vector
 		</Button>
 	)
 
 	return (
-		<div className={'app'}>
+		<div className={ 'app' }>
 			{
-				Object.values(FastFourierTransforms).map((name) => (
-					<div
-						key={name}
-						className={'scatter-chart-container'}
-					>
-						<ScatterChart
-							width={SCATTER_SIDE_LENGTH}
-							height={SCATTER_SIDE_LENGTH}
-							margin={{
-								top: SCATTER_MARGIN,
-								right: SCATTER_MARGIN,
-								bottom: SCATTER_MARGIN,
-								left: SCATTER_MARGIN,
-							}}
+				Object
+					.values(FastFourierTransforms)
+					.map((name) => (
+						<div
+							key={ name }
+							className={ 'scatter-chart-container' }
 						>
-							<CartesianGrid/>
-							<XAxis type="number" dataKey="x"/>
-							<YAxis type="number" dataKey="y"/>
-							<Scatter
-								name={name}
-								data={VECTOR_MOCKUP}
-								fill={SCATTER_FILL_COLOR}
-							/>
-						</ScatterChart>
-						<p>
-							{name}
-						</p>
-						{name === 'FFT' ? BUTTON : SLIDER}
-					</div>
-				))
+							<ScatterChart
+								width={ SCATTER_SIDE_LENGTH }
+								height={ SCATTER_SIDE_LENGTH }
+								margin={ {
+									top: SCATTER_MARGIN,
+									right: SCATTER_MARGIN,
+									bottom: SCATTER_MARGIN,
+									left: SCATTER_MARGIN,
+								} }
+							>
+								<CartesianGrid/>
+								<XAxis type="number" dataKey="x"/>
+								<YAxis type="number" dataKey="y"/>
+								<Scatter
+									name={ name }
+									data={ VECTOR_MOCKUP }
+									fill={ SCATTER_FILL_COLOR }
+								/>
+							</ScatterChart>
+							<p>
+								{ name }
+							</p>
+							{ name === 'FFT' ? BUTTON : SLIDER }
+						</div>
+					))
 			}
 		</div>
 	)
